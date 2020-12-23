0599943756440053fc4bb4254b42ca73d983871e
krista3200 <73085133+krista3200@users.noreply.github.com>
2020-12-23 15:53:44 -0600

    added second set attribute

--- script.js
+++ script.js
@@ -60,7 +60,8 @@ function displayWeather(response) {
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML=formatDate(response.data.dt * 1000);
-    iconElement.setAttribute(`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
   
                    
   }
