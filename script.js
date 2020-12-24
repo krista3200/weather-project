77c2ee7e5705d078dab924dbe5532e99bf85751b
krista3200 <73085133+krista3200@users.noreply.github.com>
2020-12-24 00:05:29 -0600

    updated js

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ function displayWeather(response) {
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
-  celsiusLink.classList.remove("active");
+  celsiusLink.attributes.remove("active");
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
