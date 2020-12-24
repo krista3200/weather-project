985e2d8bd0021f287302b53276ce262f9a220c25
krista3200 <73085133+krista3200@users.noreply.github.com>
2020-12-24 14:13:15 -0600

    updated js

--- script.js
+++ script.js
@@ -87,8 +87,8 @@ function displayWeather(response) {
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature"); 
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
+  celsiusLink.attributes.remove("active");
+  fahrenheitLink.attributes.add("active");
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
@@ -96,8 +96,8 @@ function displayFahrenheitTemperature(event) {
 function displayCelsiusTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
+  celsiusLink.attributes.add("active");
+  fahrenheitLink.attributes.remove("active");
 
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
