8d71fd3da89c088f23380ec47b7417024564d02b
krista3200 <73085133+krista3200@users.noreply.github.com>
2020-12-24 15:10:47 -0600

    updated js

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecast = response.data.list[0];
   forecastElement.innerHTML = `
+  <div class="row row-cols-1 row-cols-md-5">
   <div class="col mb-4">
   <div class="card h-100"> 
   <div class="card-body">
