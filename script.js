6c609dda020fc751a61562d1ac4f4b73fcd69f50
krista3200 <73085133+krista3200@users.noreply.github.com>
2020-12-24 15:08:05 -0600

    updated js

--- script.js
+++ script.js
@@ -46,10 +46,7 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecast = response.data.list[0];
   forecastElement.innerHTML = `
-  <div class="row weather-forecast" id="forecast">
-  <div class="row row-cols-1 row-cols-md-5">
-  <div class="col mb-4">
-  <div class="card h-100">   
+  
   <div class="card-body">
   <p class="card-text">
   <h3>${formatHours(forecast.dt * 1000)}</h3>
